7515784d69232b22dab8df98510f507b17ff07b1
vaishnavirbhat <112920991+vaishnavirbhat26@users.noreply.github.com>
2023-12-31 11:41:18 +0530

    Update StepThree.jsx

--- src/pages/register/StepThree.jsx
+++ src/pages/register/StepThree.jsx
@@ -31,9 +31,12 @@ import { useState, useEffect } from "react";
 const StepThree = () => {
     const { register, watch } = useFormContext();
     const [showWalkingOptions, setShowWalkingOptions] = useState(false);
+    const [showRunningOptions, setShowRunningOptions] = useState(false);
 
     // Watching for changes in the "goes out for walks" question
     const goesOutForWalks = watch("goesOutForWalks");
+    const goesOutForRuns = watch("goesOutForRuns");
+
 
     // Effect to set whether to show walking options based on the user's answer
     useEffect(() => {
@@ -43,6 +46,14 @@ const StepThree = () => {
             setShowWalkingOptions(false);
         }
     }, [goesOutForWalks]);
+
+    useEffect(() => {
+        if(goesOutForRuns === 'yes'){
+            setShowRunningOptions(true);
+        } else {
+            setShowRunningOptions(false);
+        }
+    }, [goesOutForRuns]);
 
     return (
         <>
@@ -62,6 +73,28 @@ const StepThree = () => {
                 />
             )}
             {/* ... other activities */}
+            <label htmlFor="goesOutForRuns" className="flex justify-between items-center mt-2 cursor-pointer">
+                <p className="text-sm font-semibold">Do you run?</p>
+                <select name="goesOutForRuns" id="goesOutForRuns" {...register("goesOutForRuns")}>
+                    <option value="no">No</option>
+                    <option value="yes">Yes</option>
+                </select>
+            </label>
+            {showRunningOptions && (
+
+                <FormActivity
+                    activity="Which pace do you prefer?"
+                    activityKey={"running"}
+                    options={["Slow", "Moderate", "Fast"]}
+                />
+            )}
+            <label htmlFor="dogWalking" className="flex justify-between items-center mt-2 cursor-pointer border border-white p-2 rounded" >
+                <p className="text-sm font-semibold" >Do you often walk dogs?</p>
+                <div className="switch" >
+                    <input type="checkbox" name="dogWalking" id="dogWalking" {...register("dogWalking")} />
+                    <span className="slider" ></span>
+                </div>
+            </label>
         </>
     )
 }
