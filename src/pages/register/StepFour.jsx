d25ed62e74c9b361eb800cd286a6d6610dc9be2e
vaishnavirbhat <112920991+vaishnavirbhat26@users.noreply.github.com>
2023-12-31 12:25:57 +0530

    Update StepFour.jsx

--- src/pages/register/StepFour.jsx
+++ src/pages/register/StepFour.jsx
@@ -1,13 +1,23 @@
 import FormActivity from "./FormActivity"
+import { useFormContext } from "react-hook-form"
 
 const StepFour = () => {
+
+    const {register} = useFormContext()
+
     return (
         <>
-            <FormActivity activity="Gardening" activityKey={"gardening"} options={["Flowers / Landscapes", "Fruits / Veggies", "Both"]} />
+            <label htmlFor="gardening" className="flex justify-between items-center mt-2 cursor-pointer border border-white p-2 rounded" >
+                <p className="text-sm font-semibold" >Are you into Gardening?</p>
+                <div className="switch" >
+                    <input type="checkbox" name="gardening" id="gardening" {...register("gardening")} />
+                    <span className="slider" ></span>
+                </div>
+            </label>
             <FormActivity activity="Swimming" activityKey={"swimming"} options={["Backyard", "Public Pool", "Health Club Pool"]} />
             <FormActivity activity="Quick Coffee / Tea Breaks" activityKey={"coffeeTea"} options={["My place", "Your Place"]} />
         </>
     )
 }
 
-export default StepFour
+export default StepFour;
